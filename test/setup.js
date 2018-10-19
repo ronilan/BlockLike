d00120dac1114875c2b3a836288f81816c2e95ff
Ron Ilan <git@ronilan.com>
2018-10-19 15:02:59 -0700

    npm update and resulting test setup change

--- test/setup.js
+++ test/setup.js
@@ -3,7 +3,15 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 // TODO: figure out jsdom resource loading paths. Setting is: { resources: "usable" }
-const { document } = (new JSDOM('')).window;
+const { document } = (new JSDOM('',
+  {
+    url: "https://example.org/",
+    referrer: "https://example.com/",
+    contentType: "text/html",
+    includeNodeLocations: true,
+    storageQuota: 10000000
+  })).window;
+
 global.document = document;
 
 Object.keys(document.defaultView).forEach((property) => {
