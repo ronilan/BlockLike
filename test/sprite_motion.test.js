6a02663afdd8d6052b779b227c03376aac56f983
Ron Ilan <github@ronilan.com>
2021-06-10 16:14:06 -0700

    Fixing flaky timed test. Give glide a better chance to complete.

--- test/sprite_motion.test.js
+++ test/sprite_motion.test.js
@@ -213,7 +213,7 @@ describe('Sprite Motion', () => {
         assert(sprite.y === 100);
         assert(Date.now() - startAt > 1000);
         done();
-      }, (sec * 1000) + 500 + (sprite.pace * 2));
+      }, (sec * 1000) + 500 + (sprite.pace * 5));
     });
   });
 });
