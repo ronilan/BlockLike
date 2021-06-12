9db61bd6e219e10f5983f6412edc80d44b5d0105
Ron Ilan <github@ronilan.com>
2021-06-12 14:31:08 -0700

    Added config file to build the temporary test package removing path from package.json

---
+++ webpack.config.test.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const glob = require('glob');
+
+const PACKAGE = require('./package.json');
+const version = PACKAGE.version;
+
+module.exports = {
+  entry: { 'test' : glob.sync('./test/*.test.js') },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: `~~out.js`,
+    path: path.resolve(__dirname, 'test'),
+  }
+};
