e42e5c1b19630e4fa026902278869bb67781a67d
Ron Ilan <github@ronilan.com>
2021-11-27 17:52:57 -0800

    Removed color from example code.

--- example/example-helper.js
+++ example/example-helper.js
@@ -1,2 +1,2 @@
 console.log(`%c  BlockLike Example Code  `, 'background: #A2DAFF; color: #222; font-size: 18px;');
-console.log(`%c${document.getElementById('example').innerHTML}`, 'color: #222; font-size: 14px;');
+console.log(`%c${document.getElementById('example').innerHTML}`, 'font-size: 14px;');
